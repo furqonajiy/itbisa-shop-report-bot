f0b91ddaa61428c041d5ef9f11c1644b2be3a0c2
Furqon Aji Yudhistira <furqonajiy@gmail.com>
2023-12-03 12:23:24 +0700

    Add constant

---
+++ generator/utility/constant.py
@@ -0,0 +1,16 @@
+BASE_DIR = "H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace"
+
+BISATRANSAKSI_DIR = [
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Bukalapak - BisaTransaksi v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaTransaksi v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaTransaksi v2'
+]
+
+BISASALDO_DIR = [
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Bukalapak - BisaSaldo v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaSaldo v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaSaldo v2'
+]
+
+BISALAPORAN_BUKALAPAK_DIR = 'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Bukalapak - BisaLaporan v2'
+BISALAPORAN_TOKOPEDIA_DIR = 'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaLaporan v2'
