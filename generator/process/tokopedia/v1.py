a6ec4f88fe88cb1c35d79963827f232823ab0ff9
Furqon Aji Yudhistira <furqonajiy@gmail.com>
2023-12-05 23:04:39 +0700

    remove logic to process bisa saldo

--- generator/process/tokopedia/v1.py
+++ generator/process/tokopedia/v1.py
@@ -2,11 +2,9 @@ import logging
 
 import pandas as pd
 
-from bisabonus.tokopedia.v1 import generate_bisabonus
 from bisainvoice.tokopedia.v1 import generate_bisainvoice
 from bisajual.tokopedia.v1 import generate_bisajual
-from bisaremit.tokopedia.v1 import generate_bisaremit
-from keywordchecker.tokopedia import check_status_keyword, check_saldo_keyword
+from keywordchecker.tokopedia import check_status_keyword
 from utility.constant import BISALAPORAN_TOKOPEDIA_DIR
 from utility.generic import create_directory
 
@@ -18,9 +16,6 @@ def process(list_report):
 
     for tkp_file in list_report:
         read_bisatransaksi(tkp_file)
-
-    for tkp_file in list_report:
-        read_bisasaldo(tkp_file)
 
 
 def read_bisatransaksi(tkp_file):
@@ -41,17 +36,3 @@ def read_bisatransaksi(tkp_file):
             check_status_keyword("1", tkp_file, df)
             generate_bisainvoice(tkp_file, df)
             generate_bisajual(tkp_file, df)
-
-
-def read_bisasaldo(tkp_file):
-    cond1 = 'BisaSaldo v1 Tokopedia' in tkp_file
-    cond2 = '~' not in tkp_file
-    if cond1 and cond2:
-        logging.debug("Read {0}".format(tkp_file))
-
-        df = pd.read_excel(tkp_file, skiprows=6)
-
-        if len(df) > 0:
-            check_saldo_keyword(tkp_file, df)
-            generate_bisaremit(tkp_file, df)
-            generate_bisabonus(tkp_file, df)
