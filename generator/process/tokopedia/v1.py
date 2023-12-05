9be1dc0e2ffac02ace873a56a1cd95ec2cd42d10
Furqon Aji Yudhistira <furqonajiy@gmail.com>
2023-12-05 22:07:18 +0700

    Add read bisa saldo

--- generator/process/tokopedia/v1.py
+++ generator/process/tokopedia/v1.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 from bisainvoice.tokopedia.v1 import generate_bisainvoice
 from bisajual.tokopedia.v1 import generate_bisajual
-from keywordchecker.tokopedia import check_status_keyword
-from utility.constant import BISALAPORAN_TOKOPEDIA_V1_DIR, BISALAPORAN_TOKOPEDIA_V2_DIR
+from keywordchecker.tokopedia import check_status_keyword, check_saldo_keyword
+from utility.constant import BISALAPORAN_TOKOPEDIA_V1_DIR
 from utility.generic import create_directory
 
 
 def process(list_report):
-    logging.info("Process Tokopedia File")
+    logging.info("Process Tokopedia v1 File")
 
     create_directory(BISALAPORAN_TOKOPEDIA_V1_DIR)
 
     for tkp_file in list_report:
         read_bisatransaksi(tkp_file)
 
-    # for tkp_file in list_report:
-    #     read_bisasaldo(tkp_file)
+    for tkp_file in list_report:
+        read_bisasaldo(tkp_file)
 
 
 def read_bisatransaksi(tkp_file):
@@ -39,3 +39,16 @@ def read_bisatransaksi(tkp_file):
             check_status_keyword("1", tkp_file, df)
             generate_bisainvoice(tkp_file, df)
             generate_bisajual(tkp_file, df)
+
+def read_bisasaldo(tkp_file):
+    cond1 = 'BisaSaldo v1 Tokopedia' in tkp_file
+    cond2 = '~' not in tkp_file
+    if cond1 and cond2:
+        logging.debug("Read {0}".format(tkp_file))
+
+        df = pd.read_excel(tkp_file, skiprows=6)
+
+        if len(df) > 0:
+            check_saldo_keyword(tkp_file, df)
+            generate_bisaremit(tkp_file, df)
+            generate_bisabonus(tkp_file, df)
