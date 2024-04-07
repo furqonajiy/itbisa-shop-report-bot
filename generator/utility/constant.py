c26cd19e5d228e5a64010a547d28976ba5be84e9
Furqon Aji Yudhistira <furqonajiy@gmail.com>
2024-04-07 19:10:09 +0700

    Add Constant Shopee V3

--- generator/utility/constant.py
+++ generator/utility/constant.py
@@ -2,7 +2,9 @@ BASE_DIR = "H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\Bis
 
 BISATRANSAKSI_DIR = [
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Bukalapak - BisaTransaksi v2',
+
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaTransaksi v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaTransaksi v3',
 
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaTransaksi v1',
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaTransaksi v2'
@@ -10,14 +12,17 @@ BISATRANSAKSI_DIR = [
 
 BISASALDO_DIR = [
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Bukalapak - BisaSaldo v2',
+
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaSaldo v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaSaldo v3',
 
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaSaldo v1',
     'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Tokopedia - BisaSaldo v2',
 ]
 
 BISAFEE_DIR = [
-    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaFee v2'
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaFee v2',
+    'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Shopee - BisaFee v3'
 ]
 
 BISALAPORAN_BUKALAPAK_DIR = 'H:\My Drive\ITBisa - 2\CBDO - Chief Business Development Officer\BisaLaporan - Marketplace\Bukalapak - BisaLaporan v2'
